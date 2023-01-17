c66db5b9638f57b7dfd70724428ec0463b0e3ee2
Alpacorp <alejandro.palacios88@gmail.com>
2023-01-17 18:09:10 -0500

    add captcha endpoint using axios library

--- routes/form.js
+++ routes/form.js
@@ -6,6 +6,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const hubspot = require("@hubspot/api-client");
+const axios = require("axios");
 
 const router = Router();
 
@@ -53,5 +54,27 @@ router.post(
     }
   }
 );
+
+router.post("/captcha", (req, res) => {
+  const { token } = req.body;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET_KEY}&response=${token}`;
+
+  axios
+    .post(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+      },
+    })
+    .then((response) => {
+      res.json({
+        message: response.data,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: error,
+      });
+    });
+});
 
 module.exports = router;
